54ab42dc634621b2dae08ef502bede998a47934e
LuHenriSouza <luizhapsouza@gmail.com>
2024-10-24 09:37:25 -0300

    preparing the environment to work with media

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { PostsService } from './posts/posts.service';
 import { PostsModule } from './posts/posts.module';
+import { MediaModule } from './media/media.module';
 
 @Module({
   imports: [
@@ -10,7 +11,8 @@ import { PostsModule } from './posts/posts.module';
       isGlobal: true,
     }),
     UsersModule,
-    PostsModule
+    PostsModule,
+    MediaModule
   ],
   providers: [PostsService],
 })
